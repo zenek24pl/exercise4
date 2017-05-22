47a3582c7dda3302a4c7f2114fb183a1de51de56
AndrzejWojtani <awojtania@9bits.com.pl>
2017-05-22 23:22:27 +0200

    JmsSender Class  - SendMapToTopic, Send text and order to queue methods implemented - 3/3 test passed

--- src/main/java/wdsr/exercise4/sender/JmsSender.java
+++ src/main/java/wdsr/exercise4/sender/JmsSender.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.util.Map;
 
 import javax.jms.Connection;
+import javax.jms.DeliveryMode;
 import javax.jms.Destination;
 import javax.jms.JMSException;
 import javax.jms.MapMessage;
@@ -39,14 +40,15 @@ public class JmsSender {
 	public void sendOrderToQueue(final int orderId, final String product, final BigDecimal price)throws JMSException {
 		// localhost:61616
 				order=new Order(orderId,product,price);
-				Connection connectionToBroker = new ActiveMQConnectionFactory("tcp://localhost:61616").createConnection();
+				Connection connectionToBroker = new ActiveMQConnectionFactory("tcp://localhost:62616").createConnection();
 				connectionToBroker.start();
 				Session session = connectionToBroker.createSession(false, Session.AUTO_ACKNOWLEDGE);
-				Destination destination = session.createQueue("trades");
+				Destination destination = session.createQueue(queueName);
 				MessageProducer producer = session.createProducer(destination);
 				ObjectMessage mg=session.createObjectMessage();
-			
 				mg.setObject(order);
+				mg.setJMSType("Order"); 
+				mg.setStringProperty("WDSR-System","OrderProcessor");
 				producer.send(mg);
 				session.close();
 				connectionToBroker.close();
@@ -62,11 +64,11 @@ public class JmsSender {
 	 */
 	public void sendTextToQueue(String text)throws JMSException {
 		// TODO
-		ActiveMQConnectionFactory connectionFactory=new ActiveMQConnectionFactory("tcp://localhost:61616");
+		ActiveMQConnectionFactory connectionFactory=new ActiveMQConnectionFactory("tcp://localhost:62616");
 		Connection connectionToBroker = connectionFactory.createConnection();
 		connectionToBroker.start();
 		Session session = connectionToBroker.createSession(false, Session.AUTO_ACKNOWLEDGE);
-		Destination destination = session.createQueue("trades");
+		Destination destination = session.createQueue(queueName);
 		MessageProducer producer = session.createProducer(destination);
 		TextMessage message = session.createTextMessage();
 		message.setText(text);
@@ -82,13 +84,15 @@ public class JmsSender {
 	 */
 	public void sendMapToTopic(Map<String, String> map)throws JMSException {
 		// TODO
-	Connection connectionToBroker = new ActiveMQConnectionFactory("tcp://localhost:61616").createConnection();
+	Connection connectionToBroker = new ActiveMQConnectionFactory("tcp://localhost:62616").createConnection();
 		connectionToBroker.start();
 		Session session = connectionToBroker.createSession(false, Session.AUTO_ACKNOWLEDGE);
-		Destination destination = session.createQueue("trades");
+		Destination destination = session.createTopic(topicName);
 		MessageProducer producer = session.createProducer(destination);
 		MapMessage message = session.createMapMessage();
-		  for (Map.Entry<String, ?> entry : map.entrySet()) message.setObject(entry.getKey(), entry.getValue());
+		  for (Map.Entry<String, ?> entry : map.entrySet()){
+			  message.setObject(entry.getKey(), entry.getValue());
+		  }
 		   
 			   producer.send(message);
 			   session.close();
